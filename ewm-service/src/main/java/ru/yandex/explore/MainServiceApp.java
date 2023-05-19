3b1a92b19f7a70a3d742d5bd27c1f97d2de6eb25
Asror <hudoykulov.asror95@gmail.com>
2023-05-19 09:41:00 +0500

    fixed 2

--- ewm-service/src/main/java/ru/yandex/explore/MainServiceApp.java
+++ ewm-service/src/main/java/ru/yandex/explore/MainServiceApp.java
@@ -2,8 +2,9 @@ package ru.yandex.explore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MainServiceApp {
 
     public static void main(String[] args) {
